190b01599387167c086b2b4d6e9eb8d6dc1071af
Jake Helfrich <jakenichelfrich@gmail.com>
2025-01-27 22:03:47 +0800

    Playing with Stereotypes & Bean configurations

--- app/app/src/main/kotlin/org/example/App.kt
+++ app/app/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,11 @@
 package org.example
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
 class App {
     val greeting: String
@@ -13,14 +17,40 @@ class App {
 @Component
 class Foo {}
 
+@Scope("prototype")
+@Service
+class Bar {}
+
+
+class Baz {}
+
+@Configuration
+open class Configuration {
+    @Bean
+    open fun baz(): Baz = Baz()
+}
+
 fun main() {
     val context = AnnotationConfigApplicationContext()
     context.scan("org.example")
     context.refresh()
 
-    val foo1 = context.getBean(Foo::class.java)
+    // Fetching Beans created from "Component"
+    val foo1 = context.getBean("foo")
     val foo2 = context.getBean(Foo::class.java)
+
+    // Default Scope is "Singleton" so only one Bean gets created and served to all clients.
     println(foo1)
     println(foo2)
+
+    // Get/Create Beans that have been created with the "Service" Annotation
+    context.getBeansWithAnnotation(Service::class.java)
+        .keys
+        .forEach { println(it) }
+
+    // Get/Create bean that was defined use Java Based Configuration
+    val baz = context.getBean("baz")
+    println(baz)
+
     println(App().greeting)
 }

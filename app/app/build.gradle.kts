135fba4fc44f26173b0fd459c13d7e6ad35a715a
Jake Helfrich <jakenichelfrich@gmail.com>
2025-01-27 20:40:56 +0800

    gradle init

---
+++ app/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.guava)
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "org.example.AppKt"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
